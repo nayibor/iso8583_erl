55d4d82b517edbfb1178c5ab6afcc33667e49064
Nuku Ameyibor <nayibor@gmail.com>
2017-04-29 02:43:44 +0000

    specs added to iso8583,iso8583_ascii
    specs added for iso8583_ascii but dialyzer not run
    export functions arguements changed in iso8583

--- src/iso8583_ascii.erl
+++ src/iso8583_ascii.erl
@@ -1,13 +1,14 @@
 %%%
-%%% @doc yapp_test_ascii_marsh_jpos module.
-%%%<br>this module is responsible for processing postillion iso messages  format</br>
+%%% @doc iso8583_ascii module.
+%%%<br>this module is responsible for processing string iso messages  format</br>
+%%%<br>currently supports postillion message formats</br>
 %%% @end
 %%% @copyright Nuku Ameyibor <nayibor@startmail.com>
 
 
 -module(iso8583_ascii).
 
--export([unpack/1,convert_base_pad/3,pad_data/3]).
+-export([unpack/3]).
 
 -define(MTI_SIZE,4).
 
@@ -28,34 +29,39 @@ fold_bin(Fun, Accum, Bin) ->
 %%this is for padding a binary up to a length of N digits with a binary character
 %%mostly used in the bitmap
 %%pad character size <2
+-spec pad_data(binary(),integer(),binary())->binary().
 pad_data(Bin,Number,Character)when is_binary(Bin),is_integer(Number),Number > 0,is_binary(Character),size(Character)<2 -> pad_data(Bin,Number,Character,Number-size(Bin)).
 pad_data(Bin,Number,Character,Counter) when Counter > 0 -> pad_data(<<Character/binary,Bin/binary>>,Number,Character,Counter-1);
 pad_data(Bin,_Number,_Character,Counter) when Counter =< 0 -> Bin.
 
 
 %% integer binary containing bitmap
+-spec convert_base(integer())->binary().
 convert_base(Data_Base_10)->
 		erlang:integer_to_binary(Data_Base_10,2).
 				
 	  	
 %%this converts data between bases and also pads the data  for our purposes
+-spec convert_base_pad(integer(),integer(),binary())->binary().
 convert_base_pad(Data_Base_10,Number_pad,Pad_digit)->
         Data_base2 = convert_base(Data_Base_10),
 		pad_data(Data_base2,Number_pad,Pad_digit).
 
 
-%% @doc this part accepts a list iso message with the header removed and extracts the mti,bitmap,data elements into a map object 
+%% @doc this part accepts a postilion list iso message with the header removed and extracts the mti,bitmap,data elements into a map object 
 %% exceptions can be thrown here if the string for the message hasnt been formatted well but they should be caught in whichever code is calling the system 
 %%the data is first converted into a binary before the processing is done . much faster and uses less memory than using lists
-unpack({list,Type,Rest})-> 
+-spec unpack(list,post,binary())->map().
+unpack(list,post,Rest)-> 
 		%%io:format("~ninit message is ~s",[Rest]),
 		Bin_message = erlang:list_to_binary(Rest),
-		process_binary(Bin_message,Type).
+		process_binary(Bin_message,post).
 
 
 %% @doc this function is used to derive various fields given an iso message and the message area(iso 1987,1992,2002,postillion,ascii subfield etc .. works with ascii )
 %% can be used for getting various iso message fields as well as getting subfields out of an iso message
 %%all data needed to calculate bitmamp should be part of input to this function 
+-spec process_binary(binary(),atom())->map().
 process_binary(Bin_message,Message_Area)->
 		{Btmptrans,Msegt,Spec_fun,Map_Init} = case Message_Area of
 								post ->
@@ -91,7 +97,7 @@ process_binary(Bin_message,Message_Area)->
 					%%io:format("~nso far ~p and field_num is ~p",[NewMap,Current_index_in]),
 					Fld_num_out = Current_index_in + 1, 
 					{Rest_bin,{Data_for_use_in,New_Index,Fld_num_out,NewMap}};
-				(<<X:1/binary, Rest_bin/binary>>, {Data_for_use_in,Index_start_in,Current_index_in,Map_out_list_in}) when X =:= <<"0">>,Current_index_in =:=1,Message_Area =:=iso_msg->
+				(<<X:1/binary, Rest_bin/binary>>, {Data_for_use_in,Index_start_in,Current_index_in,Map_out_list_in}) when X =:= <<"0">>,Current_index_in =:=1,Message_Area =:=post->
 					New_Index_fx = Index_start_in+8,
 					Fld_num_out = Current_index_in + 1,					
 					{Rest_bin,{Data_for_use_in,New_Index_fx,Fld_num_out,Map_out_list_in}};
@@ -110,9 +116,14 @@ pack(_Message_Map)->
 
 
 %% @doc this is for setting a particular field in the message
--spec set_field(map(),pos_integer(),pos_integer()|[pos_integer()]|binary())->map().
-set_field(_Map_Message,_Fld_num,_Fld_val)->
-		ok.
+-spec set_field(Iso_Map::map(),Fld_num::pos_integer(),Fld_val::term())->{ok,map()}|{error,binary()}.
+set_field(_Iso_Map,_Fld_num,_Fld_val)->
+		{ok,<<>>}.
+		
+%% @doc this is for getting a particular field in the message
+-spec get_field(Fld_num::pos_integer(),Iso_Map::map())->{ok,binary()}|{error,binary()}.
+get_field(Fld_num,Iso_Map)->
+		{ok,<<>>}.
 		
 		
 %%have to add function which will set sub field		
--- src/iso8583_erl.erl
+++ src/iso8583_erl.erl
@@ -1,21 +1,23 @@
 -module(iso8583_erl).
 
 %% API exports
--export([]).
+-export([unpack/3]).
 
 %%====================================================================
 %% API functions
 %%====================================================================
 
 
-%%for unpacking messages of various types
+%% @doc for unpacking messages of various types
+%%for now api works with only Mformat=list and InterfaceType=post 
+%%other ones will be added later 
 %%%
--spec unpack(Mformat,InterfaceType,IsoMessage) ->ok|{error,any()} when
+-spec unpack(Mformat,InterfaceType,IsoMessage) ->map()|{error,any()} when
 	Mformat  		:: list,
 	InterfaceType  	:: post,
 	IsoMessage		:: [integer()].	
 unpack(Mformat,InterfaceType,IsoMessage)-> 
-		iso8583_ascii:unpack({Mformat,InterfaceType,IsoMessage}).
+		iso8583_ascii:unpack(Mformat,InterfaceType,IsoMessage).
 
 
 
