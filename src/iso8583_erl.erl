8f8522c2631253993fbf2597c525d88aee20882b
Nuku Ameyibor <nayibor@gmail.com>
2019-01-30 17:34:05 +0000

    bug fixes
    code added to process bitmaps for binaries correctly
    code added to process bitmaps which are hex
    code for packing and unpacking messages split up and made more modular

--- src/iso8583_erl.erl
+++ src/iso8583_erl.erl
@@ -1,21 +1,13 @@
 -module(iso8583_erl).
 
 %% API exports
--export([unpack/3,pack/2]).
+-export([unpack/2,pack/2]).
 
 %%====================================================================
 %% API functions
 %%====================================================================
 
 
-%% @doc for unpacking messages
-
--spec unpack(Mformat,Module_process,IsoMessage) ->map()|{error,any()} when
-	Mformat  			:: list|binary,
-	Module_process  	:: atom,
-	IsoMessage			:: [integer()].	
-unpack(Mformat,Module_process,IsoMessage)-> 
-		iso8583_ascii:unpack(Mformat,Module_process,IsoMessage).
 
 
 %% @doc for packing messages into iso list format
@@ -24,6 +16,18 @@ unpack(Mformat,Module_process,IsoMessage)->
 	Module_process :: atom.
 pack(Map_pack,Module_process)->
 	iso8583_ascii:pack(Map_pack,Module_process).
+
+
+%% @doc for unpacking messages
+-spec unpack(IsoMessage,Module_process) ->map()|{error,any()} when
+	IsoMessage			:: [integer()],
+	Module_process  	:: atom.
+
+unpack(IsoMessage,Module_process)-> 
+	iso8583_ascii:unpack(IsoMessage,Module_process).
+
+
+
 
 %%====================================================================
 %% Internal functions
