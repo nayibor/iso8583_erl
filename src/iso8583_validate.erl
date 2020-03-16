5d9ae8f62a886584d1ee72c69df8458567c8de67
Nuku Ameyibor <nayibor@gmail.com>
2020-03-16 16:07:02 +0000

    validation for numbers,floating point,alphabet,numerics
    validation for number,floating point,alphabets,numberics worked on
    this validation also includes signed floating points/signed numbers
    and numbers

--- src/iso8583_validate.erl
+++ src/iso8583_validate.erl
@@ -7,7 +7,7 @@
 
 -module(iso8583_validate).
 
--export([validate_data/2]).
+-export([validate_data/2,perform_validation/3]).
 
 
 %% @doc this is for validating an iso8583 message for incoming messages based on a specification
@@ -20,8 +20,7 @@ validate_data(Data_map,Specification_map)->
 			Result_validation = validate_data_sub({Value_iso,Data_type,Flength,Fx_var_fixed,Fx_header_length,Sub_format}),
 			case Result_validation of
 				ok ->
-					Final_form = iso8583_ascii:get_data_element(Value_iso,Data_type),
-					New_result_map = maps:put(Key_iso,Final_form,Map_result),
+					New_result_map = maps:put(Key_iso,Value_iso,Map_result),
 					{{ok,New_result_map},{error,Error_list}};
 				{error,Result} ->
 					{{ok,Map_result},{error,[{Key_iso,Result}|Error_list]}}
@@ -34,51 +33,124 @@ validate_data(Data_map,Specification_map)->
 
 
 
-validate_data_sub({Data,Data_type,Flength,Fx_var_fixed,Fx_header_length,Sub_format})->
-	perform_validation(Data,Data_type,Sub_format,Flength).
+validate_data_sub({Data,Data_type,Flength,Fx_var_fixed,Fx_header_length,Sub_format})when  
+	Fx_var_fixed =:= vl  andalso  erlang:size(Data) =< Flength andalso erlang:size(Data) > 0->
+		perform_validation(Data,Data_type,Sub_format);
+
+validate_data_sub({Data,Data_type,Flength,Fx_var_fixed,Fx_header_length,Sub_format})when  
+	Fx_var_fixed =:= fx  andalso  erlang:size(Data) =:= Flength andalso erlang:size(Data) > 0->
+		perform_validation(Data,Data_type,Sub_format);
 
 
-%% @doc for performing validation on each data element 
--spec perform_validation(tuple(),list(),list(),non_neg_integer())-> ok | {error,binary()}.
-perform_validation(Data,"N","N",Flength)->
-	case run_regex(Data,"^[0-9]*$",[]) =:= ok andalso erlang:size(Data) =< Flength of 
+validate_data_sub(_)->
+	{error,<<"Length Error">>}.	
+
+
+%% @doc for performing validation on each data element  for a number
+-spec perform_validation(binary(),list(),list())-> ok | {error,binary()}.
+perform_validation(Data,"N","N")  ->
+	case run_regex(Data,"^[0-9]*$",[]) of 
 		true ->
 			ok;
 		false ->
 			{error,<<"Format Error">>}
 	end;
-perform_validation(Data,"N","SN",_Flength)->
+
+
+%% @doc for performing validation on each data element for a signed number beginning with C or D
+perform_validation(Data,"N","SN")->
+	 case run_regex(Data,"^[C|D][0-9]*$",[]) of
+		true ->
+			ok;
+		false ->
+			{error,<<"Format Error">>}
+	end;		
+
+
+%%for performinig validaton for a floating point numberic digit without  +\-
+perform_validation(Data,"N","NF")->
+	case run_regex(Data,"^[0-9]*\.?[0-9]+$",[]) of
+		true ->
+			ok;
+		false ->
+			{error,<<"Format Error">>}
+	end;	
+
+%%for performinig validaton for a signed floating point numeric digit number beginning wiht a credit/debit without +|-
+perform_validation(Data,"N","SNF")->
+	case run_regex(Data,"^[C|D][0-9]*\.?[0-9]+$",[]) of
+		true ->
+			ok;
+		false ->
+			{error,<<"Format Error">>}
+	end;	
+
+
+%%fr performing regex of alphabets
+perform_validation(Data,"A","A")->
+	case run_regex(Data,"^[a-zA-Z]*$",[]) of
+		true ->
+			ok;
+		false ->
+			{error,<<"Format Error">>}
+	end;
+
+
+
+%% for performing alpha numeric checks
+perform_validation(Data,"AN","AN")->
+	case run_regex(Data,"^[a-zA-Z0-9]*$",[]) of
+		true ->
+			ok;
+		false ->
+			{error,<<"Format Error">>}
+	end;
+
+%% for performing alpha numeric checks
+perform_validation(Data,"ANS","ANS")->
 	ok;
-perform_validation(_Data,"N","NF",_Flength)->
+
+
+perform_validation(_Data,"S","S")->
 	ok;
-perform_validation(__Data,"N","MMDDhhmmss",_Flength)->
+
+
+perform_validation(_Data,"NS","NS")->
 	ok;
-perform_validation(_Data,"N","hhmmss",_Flength)->
+
+
+perform_validation(_Data,"B","B")->
 	ok;
-perform_validation(_Data,"N","MMDD",_Flength)->
+
+
+perform_validation(Data=[MM,DD,HH,MM,SS],"N","MMDDhhmmss")->
 	ok;
-perform_validation(_Data,"N","YYMM",_Flength)->
+
+perform_validation(Data,"N","hhmmss")->
+	run_regex(Data,"",[]);
+
+
+perform_validation(__Data,"N","YYMMddhhmmss")->
 	ok;
-perform_validation(_Data,"N","YYMMDD",_Flength)->
+
+perform_validation(_Data,"N","MMDD")->
 	ok;
-perform_validation(_Data,"NS","NS",_Flength)->
+
+perform_validation(_Data,"N","YYMM")->
 	ok;
-perform_validation(_Data,"A","A",_Flength)->
+	
+perform_validation(_Data,"N","YYMMDD")->
 	ok;
-perform_validation(_Data,"ANS","ANS",_Flength)->
-	ok;
-perform_validation(_Data,"AN","AN",_Flength)->
-	ok;
-perform_validation(_Data,"S","S",_Flength)->
-	ok;
-perform_validation(_Data,"B","B",_Flength)->
-	ok;
-perform_validation(_Data,_,_,_)->
-	{error,<<"unknown format">>}.
+
+
+
+perform_validation(_,_,_)->
+	{error,<<"Format Error Or Length Error">>}.
 
 
 run_regex(Data,Regex_Expression,Options)->
   case re:run(Data, Regex_Expression,Options) of
-      nomatch    -> ok;
-      {match, _} -> error
+      match -> true;
+      {match, _} -> true;
+      _ -> false
   end.
