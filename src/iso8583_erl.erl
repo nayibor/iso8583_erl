8d0ebe2ee3aeff497c02f901e7eb96f6acd735e2
Nuku Ameyibor <nayibor@gmail.com>
2020-05-21 01:31:51 +0000

    readme modified and docs modified
    readme modified and some portions of documentation corrected

--- src/iso8583_erl.erl
+++ src/iso8583_erl.erl
@@ -1,7 +1,13 @@
+%%%
+%%% @doc iso8583_erl module.
+%%%<br>this module is the api for library </br>
+%%% @end
+%%% @copyright Nuku Ameyibor <nayibor@startmail.com>
+
 -module(iso8583_erl).
 
 %% API exports
--export([unpack/2,pack/2,get_size/2,get_field/2,set_field/3,set_field_list/1,set_mti/2,process_data_element/4,validate_data/3,
+-export([unpack/2,pack/2,get_size/2,get_field/2,set_field/3,set_field_list/1,set_mti/2,process_data_element/4,
 		 create_bitmap/2,get_bitmap_subs/3,get_size_send/2,load_specification/1,get_spec_field/2,get_bitmap_type/1]).
 
 %%====================================================================
@@ -9,7 +15,7 @@
 %%====================================================================
 
 
-%% @doc creats a new map specification which contains the various data elements and a bitmap from a specification file
+%% @doc creats a  specification which contains the various data elements and their specifications from a spec file
 -spec load_specification(string() |binary())->map().
 load_specification(Filename)->
 	iso8583_process:load_specification(Filename).
@@ -21,13 +27,13 @@ get_spec_field(Field,Specification)->
 	iso8583_process:get_spec_field(Field,Specification).
 
 
-%% @doc gets the bitmap type
+%% @doc gets the bitmap type from a specification
 -spec get_bitmap_type(map())->hex|binary.
 get_bitmap_type(Specification)->
 	iso8583_process:get_bitmap_type(Specification).
 
 
-%% @doc for packing messages into iso list format
+%% @doc for packing messages into an iolist format
 -spec pack(Map_pack,Specification)->list()|{error,term()}when
 	Map_pack ::map(),
 	Specification :: map().
@@ -35,7 +41,7 @@ pack(Map_pack,Specification)->
 	iso8583_process:pack(Map_pack,Specification).
 
 
-%% @doc for unpacking messages
+%% @doc for unpacking messages into a map object
 -spec unpack(IsoMessage,Specification) ->map()|{error,any()} when
 	IsoMessage		:: [integer()],
 	Specification  	:: map().
@@ -44,16 +50,7 @@ unpack(IsoMessage,Specification)->
 	iso8583_process:unpack(IsoMessage,Specification).
 
 
-
-%% @doc this is for validating an iso8583 message for incoming/outgoing messages based on a  loaded specification
-%% specification should have been loaded
-%% @doc De_type,Length_field,Fl_vl,Header_length,Format
--spec validate_data(map(),map(),out|in)->{{ok,map()},{error,list()}}.
-validate_data(Data_map,Specification_map,Out_or_in)->
-	iso8583_validate:validate_data(Data_map,Specification_map,Out_or_in).
-
-
-%%  @doc for getting the final size of a bitmap or field list
+%%  @doc for getting the size of a bitmap or an iolist of fields
 -spec get_size(Type,Value) ->integer() when
 	Type    :: bitmap|field_list,
 	Value  	:: iolist()|binary()|list().
@@ -74,13 +71,13 @@ set_field(Iso_Map,Fld_num,Fld_val)->
 	iso8583_process:set_field(Iso_Map,Fld_num,Fld_val).
 
 
-%%  @doc for setting the fields but accepts a list of fields and returns a map containing output
+%%  @doc for setting a list of fields
 -spec set_field_list(List::list())->map().
 set_field_list(List)->
 	iso8583_process:set_field_list(List).
 
 
-%%  @doc for setting the fields for an iso message
+%%  @doc for setting the a single field for an iso message
 -spec set_mti(Iso_Map::map(),Fld_val::term())->{ok,map()}|{error,term()}.
 set_mti(Iso_Map,Mti_val)->
 	iso8583_process:set_mti(Iso_Map,Mti_val).
@@ -93,23 +90,21 @@ process_data_element(Bitmap,Index_start,Data_binary,Specification)->
 	iso8583_process:process_data_element(Bitmap,Index_start,Data_binary,Specification).
 
 
-%%for creating the final bitmap
-%%this bitmap is an 8/16 byte binary with each byte being represented  by an integer.
-%%integer converted to a an 2 bit binary represents presence or absence of those fields
+%% @doc for creating the bitmap of a message
 -spec create_bitmap(binary|hex,binary())->binary()|list().
 create_bitmap(Type_bitmap,Bitmap_final_bit)->
 	iso8583_process:create_bitmap(Type_bitmap,Bitmap_final_bit).
 
 
 
-%% @doc forr getting the bitmap,mti,Data fields 
+%% @doc for getting the bitmap,mti,Data fields of an iso message
 -spec get_bitmap_subs(atom(),binary(),map())-> tuple().
 get_bitmap_subs(Bitmap_type,Bin_message,Specification)->
 	iso8583_process:get_bitmap_subs(Bitmap_type,Bin_message,Specification).
 
 
 
-%%for getting the final size of the message to be sent 
+%%for getting the size of the message to be sent 
 %%-spec get_size_send(binary(),binary()|list(),list())->non_neg_integer().
 -spec get_size_send(iolist(),non_neg_integer())->list(). 
 get_size_send(Fields_iolist,Length_max_size)->
